f0c8c2df04091ad1e852d9311d3eb13c888a9735
Kami <72298687+Ent0o0on@users.noreply.github.com>
2022-12-29 19:06:20 +0100

    Add files via upload

---
+++ Numbers-to-Diagram/statistic.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+# List of numbers to plot
+numbers = [1, 9, 6, 4, 8]
+
+# Create a red, dashed line graph
+plt.plot(numbers, color='red', linestyle='dashed')
+
+# Show the graph
+plt.show()
